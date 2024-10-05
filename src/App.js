8cf11677e62ad62d6629c518557bde6f4713b63c
Danielle Purdes <daniellepurdes@gmail.com>
2024-10-05 09:09:30 -0700

    Merge PortfolioItem and PortfolioItemItchio

--- src/App.js
+++ src/App.js
@@ -37,31 +37,24 @@ function NavItem( {name, image, title, url} ) {
   );
 }
 
-function PortfolioItem( {name, videoUrl, landingUrl, description, tags} ) {
+function PortfolioItem( {name, year, image, videoUrl, landingUrl, itchioUrl, embedUrl, description, tags} ) {
   let landingLink = landingUrl ? <a href={landingUrl}>Visit {name} Site</a> : "";
+  let yearText = year ? " (" + year + ")" : "";
+  let imageElement = image ? <img className="portfolio-media" src={image} alt="Gameplay screenshot of " name/> : "";
+  let videoEmbed = videoUrl ? 
+    <iframe className="portfolio-media" width="560" height="315" src={videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" rel="0" allowfullscreen></iframe> : "";
+  let itchioEmbed = itchioUrl && embedUrl ? <iframe title={name + " - itch.io"} frameborder="0" src={embedUrl} width="552" height="167"><a href={itchioUrl}>{name}</a></iframe> : "";
 
   return (
     <div className="portfolio-element">
-      <iframe className="portfolio-media" width="560" height="315" src={videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" rel="0" allowfullscreen></iframe>
+      {imageElement}
+      {videoEmbed}
       <div className="portfolio-text">
-        <h3>{name}</h3>
+        <h3>{name}{yearText}</h3>
+        <PortfolioTags tags={tags} />
         <p>{description}</p>
         {landingLink}
-        <PortfolioTags tags={tags} />
-      </div>
-    </div>
-  );
-}
-
-function PortfolioItemItchio( {name, image, itchioUrl, embedUrl, description, tags} ) {
-  return (
-    <div className="portfolio-element">
-      <img className="portfolio-media" src={image} alt="Gameplay screenshot of Blastium"/>
-      <div className="portfolio-text">
-        <h3>{name}</h3>
-        <p>{description}</p>
-        <PortfolioTags tags={tags} />
-        <iframe title="Blastium - itch.io" frameborder="0" src={embedUrl} width="552" height="167"><a href={itchioUrl}>{name}</a></iframe>
+        {itchioEmbed}
       </div>
     </div>
   );
@@ -73,7 +66,7 @@ function PortfolioTags( {tags} ) {
   );
 
   return (
-    <ul>
+    <ul className="tag-list">
       {tagList}
     </ul>
   );
@@ -101,13 +94,15 @@ function App() {
       <NavBar items={navItems}/>
       <div class="portfolio">
         <PortfolioItem 
-          name="Tea Time"
+          name="Tea Room Simulator"
+          year="In Development"
           description="From harvesting tea leaves to brewing the perfect cup of tea, run an independent tea house your way in Tea Time. Currently in Alpha Development."
           landingUrl="https://danipurdes.github.io/playteatime/"
           tags= {["Godot", "Blender"]}
         />
-        <PortfolioItemItchio
+        <PortfolioItem
           name="Blastium"
+          year="2017"
           image={blastium_screenshot}
           itchioUrl="https://danipurdes.itch.io/blastium"
           embedUrl="https://itch.io/embed/126687?linkback=true"
@@ -115,19 +110,22 @@ function App() {
           tags={["Lua", "Love2D"]}
         />
         <PortfolioItem 
-          name="Squid Mantazord" 
+          name="Squid Mantazord"
+          year="2017" 
           videoUrl="https://www.youtube-nocookie.com/embed/jA9G3lyVIXA?si=cP6nggU-Zdb6jOLg"
           description="A top-down adventure game about a small town octopus fighting his way through a gauntlet of sea-creatures to become the next ruler of the sea, the next Squid Mantazord. Gameplay features a melee twist on twin-stick shooters, with spears you can extend in 8 directions. Made in 48 hours for Global Game Jam 2017. Co-developer: Scott Munro." 
           tags= {["C#", "Unity"]}
         />
         <PortfolioItem
           name="Sudden Death"
+          year="2016"
           videoUrl="https://www.youtube-nocookie.com/embed/l6NEmvVBqJk?si=2ZZKdr20_e75Yxu2"
           description="Assemble your party from a roster of 9 fighters each with unique stats. Then, use Physical and Special moves, the Guard ability, and various Items to win the battle. Turn-based Combat project inspired by games like the Final Fantasy series. Student project for Game Technology (CS354R) at UT Austin. Co-developers: Patrick Liu, Scott Munro."
           tags={["C++", "GLSL", "Ogre3D", "Bullet Physics"]}
         />
         <PortfolioItem
           name="Labyrinth"
+          year="2015"
           videoUrl="https://www.youtube-nocookie.com/embed/sFsm_7buEzA?si=F1gDPMrsV1rkn-tg" 
           description="Escape from the procedurally-generated maze before the Minotaur gets you! Awarded 'Best Programming' from EGaDs Fall 2015 Game Jam for the procedural maze generation algorithm. Co-developer: Scott Munro."
           tags={["C#", "Unity"]}
